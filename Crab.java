ad945ab1ca7c20d9cec2feaf119bdb8fc0e1aef8
kaanb1ack <89489424+kaanb1ack@users.noreply.github.com>
2021-08-30 15:03:00 -0700

    Crab Activity 1 completed

--- Crab.java
+++ Crab.java
@@ -2,12 +2,39 @@ import greenfoot.*;
 
 /**
  * This class defines a crab. Crabs live on the beach.
+ * @author: Kaan Black
+ * @version: Date
  */
 public class Crab extends Actor
 {
+    //This method repeats the following actions 
     public void act()
     {
-        // Add your action code here
+        move(3);
+        turnAtEdge();
+        checkKeyPress();
+        onCollision();
+    }
+
+    // Turns the Crab at the edge
+    private void turnAtEdge( )
+    {
+        if(isAtEdge())
+        {
+            turn(50);
+        }
+    }
+
+    // Checks for user key presses so user can turn the Crab
+    private void checkKeyPress( )
+    {
+        
+    }
+
+    // Checks for collisions with other objects
+    private void onCollision( )
+    {
+    
     }
 }
 
