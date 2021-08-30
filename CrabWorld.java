a7d458f6773c5304b10bd1d5a3cd5269b9798d4b
kaanb1ack <89489424+kaanb1ack@users.noreply.github.com>
2021-08-30 15:39:52 -0700

    completed crab activity 3

--- CrabWorld.java
+++ CrabWorld.java
@@ -11,6 +11,7 @@ public class CrabWorld extends World
         super(560, 560, 1);
         prepare();
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
@@ -19,5 +20,21 @@ public class CrabWorld extends World
     {
         Crab crab = new Crab();
         addObject(crab,137,276);
+        Worm worm = new Worm();
+        addObject(worm,332,110);
+        Worm worm2 = new Worm();
+        addObject(worm2,43,52);
+        Worm worm3 = new Worm();
+        addObject(worm3,147,486);
+        Worm worm4 = new Worm();
+        addObject(worm4,551,7);
+        Worm worm5 = new Worm();
+        addObject(worm5,362,432);
+        Worm worm6 = new Worm();
+        addObject(worm6,204,267);
+        Worm worm7 = new Worm();
+        addObject(worm7,80,394);
+        Worm worm8 = new Worm();
+        addObject(worm8,443,299);
     }
 }
